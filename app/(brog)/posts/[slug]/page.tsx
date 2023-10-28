a1413043fa978fc45a76a7e2f81532feefa9c08f
yikZero <i@yikzero.com>
2023-10-28 23:35:36 +0800

    fix(Post): 修复页面标题未随文章调整问题

--- app/(brog)/posts/[slug]/page.tsx
+++ app/(brog)/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { SanityDocument } from "next-sanity";
+import BrogConfig from "@/brog.config";
 import Post from "@/app/components/Post";
 import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/sanityFetch";
@@ -6,14 +7,22 @@ import { client } from "@/sanity/lib/client";
 
 // Prepare Next.js to know which routes already exist
 export async function generateStaticParams() {
-
   const posts = await client.fetch(postPathsQuery);
-
   return posts;
 }
 
-export default async function Page({ params }: { params: any }) {
-  const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
+async function fetchPost(params: any) {
+  return await sanityFetch<SanityDocument>({ query: postQuery, params });
+}
 
+export async function generateMetadata({ params }: { params: any }) {
+  const post = await fetchPost(params);
+  return {
+    title: `${post.title}｜${BrogConfig.WEB_TITLE}`,
+  };
+}
+
+export default async function PostPage({ params }: { params: any }) {
+  const post = await fetchPost(params);
   return <Post post={post} />;
 }
--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,16 +1,6 @@
-"use client";
-
-import BrogConfig from "@/brog.config";
 import PostAuthor from "./PostAuthor";
 import { SanityDocument } from "next-sanity";
 import { PortableText } from "@portabletext/react";
-
-// 动态获取文章标题
-export async function generateMetadata({ post }: { post: SanityDocument }) {
-  return {
-    title: `${post.title}｜${BrogConfig.WEB_TITLE}`,
-  }
-}
 
 export default function Post({ post }: { post: SanityDocument }) {
   return (
