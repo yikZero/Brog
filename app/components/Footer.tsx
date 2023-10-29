28385b99beaed982ecd5103e0433dd713864b05e
yikZero <i@yikzero.com>
2023-10-29 22:23:35 +0800

    perf(Contrast): 提升 Footer 文本的对比度

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,13 +5,13 @@ import { BrogLogo } from "./Icons";
 export default function Footer() {
   return (
     <>
-      <footer className="flex flex-row justify-between pt-4 pb-8 font-normal text-sm border-t border-gray-100 dark:border-gray-900">
-        <div className="text-gray-400 dark:text-gray-600">Copyright {BrogConfig.SINCE || new Date().getFullYear()} {BrogConfig.AUTHOR}</div>
+      <footer className="flex flex-row justify-between pt-4 pb-8 font-normal text-sm border-t border-gray-200 dark:border-gray-800">
+        <div className="text-gray-500 dark:text-gray-600">Copyright {BrogConfig.SINCE || new Date().getFullYear()} {BrogConfig.AUTHOR}</div>
         <div className="flex flex-row justify-center gap-1 group">
-          <div className="text-gray-300 dark:text-gray-700">Powered by</div>
+          <div className="text-gray-500 dark:text-gray-600">Powered by</div>
           <div className="flex flex-row gap-[2px] items-center">
             <BrogLogo className="w-3 h-3 fill-gray-400 group-hover:fill-blue-500 dark:fill-gray-600 group-hover:dark:fill-blue-500"/>
-            <Link href="https://github.com/yikZero/Brog-Sanity" target="_blank" className="text-gray-400 group-hover:text-blue-500 dark:text-gray-600 font-normal text-sm">Brog</Link>
+            <Link href="https://github.com/yikZero/Brog-Sanity" target="_blank" className="text-gray-500 group-hover:text-blue-500 dark:text-gray-600 font-normal text-sm">Brog</Link>
           </div>
         </div>
       </footer>
