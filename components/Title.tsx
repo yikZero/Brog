e15652f804b467374adfd9ecd649c2c2966b93d5
yikZero <i@yikzero.com>
2023-11-01 13:52:39 +0800

    perf(UX): 调整更多按钮触控区域

--- components/Title.tsx
+++ components/Title.tsx
@@ -11,7 +11,7 @@ export default function Title({ title, href, showMore = true }: TitleProps) {
     <div className={`flex flex-row justify-between items-center pb-2 ${(!href || !showMore) && 'justify-start'}`}>
       <h1 className="text-base text-gray-800 dark:text-gray-300 font-semibold">{title}</h1>
       {showMore && href && (
-        <Link className="text-sm text-gray-600 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500" href={href}>更多</Link>
+        <Link className="text-sm text-gray-600 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500 relative before:absolute before:-inset-x-3 before:-inset-y-2" href={href}>更多</Link>
       )}
     </div>
   );
