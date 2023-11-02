826d08d576132d0b9a8150bca8c2c7e086893911
yikZero <i@yikzero.com>
2023-11-02 23:11:33 +0800

    perf(Post): 补充类别为空时的处理

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,18 +3,18 @@ import { PortableText } from "@portabletext/react";
 import components from "@/components/PortableTextComponents";
 
 export default function PostPage({ post }: { post: Post }) {
-  const { title, publishedAt, categories, body } = post;
+  const { title, publishedAt, categories = [], body } = post;
 
   return (
     <main className="prose dark:prose-invert">
       <section className="flex flex-col border-b border-gray-200 dark:border-gray-800">
         <h1 className="leading-tight mb-4">{title}</h1>
         <div className="flex flex-row gap-4">
-          <div className="text-gray-600 dark:text-gray-400">
+          <div className="text-gray-600 dark:text-gray-400 mb-4">
             {publishedAt.toLocaleString()}
           </div>
           {categories && (
-            <div className="flex flex-row gap-2 mb-4">
+            <div className="flex flex-row gap-2">
               {categories.map((category) => (
                 <div
                   key={category}
