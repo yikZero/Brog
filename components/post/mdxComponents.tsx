5b19f0bca1b0e27e1e2743ecbf7576d6482297d7
yikZero <i@yikzero.com>
2023-11-12 00:49:19 +0800

    perf(img): 调整 img 实现方式

--- components/post/mdxComponents.tsx
+++ components/post/mdxComponents.tsx
@@ -2,7 +2,6 @@
 import type { MDXComponents } from "mdx/types";
 
 import Link from "next/link";
-import Image from "next/image";
 
 function clsx(...args: any) {
   return args.filter(Boolean).join(" ");
@@ -23,20 +22,23 @@ const mdxComponents: MDXComponents = {
   code: ({ className, ...props }) => (
     <code className={clsx("not-prose", className)} {...props}></code>
   ),
-  img: ({ className, ...props }) => (
-    <Image
-      alt={props.alt || ""}
-      width={0}
-      height={0}
-      sizes="100vw"
+  img: ({
+    className,
+    alt,
+    ...props
+  }: React.ImgHTMLAttributes<HTMLImageElement>) => (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
       className={clsx(
         "w-full h-auto m-0 drop-shadow-sm rounded dark:brightness-90 dark:transition-[filter] dark:hover:brightness-100",
         className
       )}
-      placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAArwAAAGQAQMAAAB28iHQAAAAA1BMVEXt7e0zK21NAAAAOElEQVR42u3BAQEAAACCoP6vbojAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEg7ixAAAXqv4zAAAAAASUVORK5CYII="
+      decoding="async"
+      loading="lazy"
+      alt={alt}
       {...props}
     />
-  )
+  ),
 };
 
 export default mdxComponents;
